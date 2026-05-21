e6dbca801f43308da0422ddfa3c10eaeca437df5
Nitin Shankar Madhu <nmadhu@umich.edu>
2026-05-21 00:20:38 -0400

    chore(agent-next): export all types from barrel

--- agent-next/src/index.ts
+++ agent-next/src/index.ts
@@ -1,4 +1,7 @@
 export { AgentStore } from "./store.js";
+export type { AgentSnapshot, AgentPhase, ToolExecution } from "./store.js";
 export { bridgeEvents } from "./bridge.js";
 export { runNextAgentSmoke, createNextAgent } from "./adapter.js";
+export type { NextAgentResult } from "./adapter.js";
 export { InkExtensionUIContext } from "./ui-context.js";
+export type { DialogRequest, Notification, InkUIState } from "./ui-context.js";
